518f8e709aa9b95145c8056c1eb93a09c5384a86
Chidimma Nworah <78983600+chidimmaNworah@users.noreply.github.com>
2023-07-28 10:57:08 +0100

    added redirect button

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { Store } from '../Store';
 
 export default function ProtectedRoute({ children }) {
+  const { search } = useLocation();
+  const redirectInUrl = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInUrl ? redirectInUrl : '/';
   const { state } = useContext(Store);
   const { userInfo } = state;
-  return userInfo ? children : <Navigate to="/signin" />;
+  return userInfo ? children : <Navigate to={`/signin?redirect=${redirect}`} />;
 }
